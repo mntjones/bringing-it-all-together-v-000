da3baadb6c9f1ac6514f969940715a99f71f6d8f
Monica Tamaru Jones <mntamaru@gmail.com>
2018-04-03 17:30:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,5 +12,9 @@ class Dog
     DB[:conn].execute("CREATE TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT")
   end
   
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+  end
+  
   
 end

20306352021f1721db2ab7e59965e00a75bbbab4
Monica Tamaru Jones <mntamaru@gmail.com>
2018-04-03 17:28:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+  
+  def initialize(id=nil, name, breed)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+  
+  def create_table
+    DB[:conn].execute("CREATE TABLE IF NOT EXISTS dogs")
+  end
+  
   
 end
